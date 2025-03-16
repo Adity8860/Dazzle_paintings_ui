7f4c5c7bf2ff601ec60e827d41efd3d23fe8f41c
AdityaGaur008 <gauradi355@gmail.com>
2025-03-16 17:53:00 +0530

    modify vite file

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,26 @@
-import { defineConfig ,loadEnv} from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // For alias configuration
+import path from "path";
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  assetsInclude: ['**/*.PNG'] ,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Add this line
-=======
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
     server: {
-      port: parseInt(env.VITE_PORT) || 3000, // ✅ Custom port from .env
+      port: parseInt(env.VITE_PORT) || 3000,
       open: true,
       strictPort: true,
->>>>>>> 1c2ee4e26f662628cc475c6ea1008ccab5730be2
     },
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"), // ✅ Allows imports like "@/components/Button"
+        "@": path.resolve(__dirname, "src"),
       },
     },
     plugins: [
       react(), 
-       tailwindcss() 
+      tailwindcss()
     ],
+    assetsInclude: ['**/*.PNG']
   };
 });

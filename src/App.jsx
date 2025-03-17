f50f551f54b1014cc38804aa9df1be57c4e3bd41
dheeraj1900 <monu.196d@gmail.com>
2025-03-18 01:41:26 +0530

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import Main from './Pages/Main'
 import Footer from './components/Footer/Footer'
 import ContactForm from './components/Contact/ContactForm'
-import ScrollToTop from './components/Scrolltotop'
+import ScrollToTop from './components/ScrollToTop'
 import About from './components/About/About.jsx'
 
 function App() {
